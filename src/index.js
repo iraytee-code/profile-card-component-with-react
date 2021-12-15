a5483951f066feaf74c825b276b52f715d21726d
iraytee-code <adeniranhope21@gmail.com>
2021-12-15 10:15:04 +0100

    created a profile card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Heading from './profile-card-components/Heading';
+import MainHeading from './profile-card-components/MainHeading';
+import CardFooter  from './profile-card-components/CardFooter';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function CardWrapper() {
+  return (
+  <div className="card">
+    <div className="cardEffect">
+    <div className="card-info">
+    <Heading />
+      <MainHeading />
+    </div>
+    <div className="myFooter">
+    <CardFooter />
+  </div>
+    </div>
+    
+  </div>
+  
+  
+  );
+  }
+  
+  // render tweet component to the dom
+  ReactDOM.render(<CardWrapper />,
+  document.querySelector('#root'));
+  
+  
---
+++ src/profile-card-components/Heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Heading() {
+    return (
+    <div className=".heading">
+        <small class="card-meta">Thought Leadership</small>
+    </div>
+    );
+    }
+
+    export default Heading;
